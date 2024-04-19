de71f2022269c50c6fb732b0018aabe164b4c74e
HaimarAr <hai_ari_mor@goierrieskola.org>
2024-04-19 12:02:05 +0200

    guri buruz bukatuta diseñua eta traductorea falta dira

--- WEB3/src/required/define.php
+++ WEB3/src/required/define.php
@@ -1,10 +1,4 @@
 <?php
-
-//Asier
-define('APP_DIR', $_SERVER['DOCUMENT_ROOT'] . '/Desktop/HolandaFantasy/WEB3/'); //Aplikazioaren karpeta edozein lekutatik atzitzeko.
-define('HREF_VIEWS_DIR', '/Desktop/HolandaFantasy/WEB3/src/views'); //Aplikazioaren views karpeta edozein lekutatik deitzeko
-define('HREF_SRC_DIR', '/Desktop/HolandaFantasy/WEB3/src'); //Aplikazioaren views karpeta edozein lekutatik deitzeko
-
     //Asier
     //define('APP_DIR', $_SERVER['DOCUMENT_ROOT'] . '/Desktop/HolandaFantasy/WEB3/'); //Aplikazioaren karpeta edozein lekutatik atzitzeko.
     //define('HREF_VIEWS_DIR', '/Desktop/HolandaFantasy/WEB3/src/views'); //Aplikazioaren views karpeta edozein lekutatik deitzeko
@@ -15,3 +9,4 @@ define('HREF_SRC_DIR', '/Desktop/HolandaFantasy/WEB3/src'); //Aplikazioaren view
     define('APP_DIR', $_SERVER['DOCUMENT_ROOT'] . '/GarapenIngurunea/ProjectManager/WEB/holanda-fantasy/WEB3/'); //Aplikazioaren karpeta edozein lekutatik atzitzeko.
     define('HREF_VIEWS_DIR', '/GarapenIngurunea/ProjectManager/WEB/holanda-fantasy/WEB3/src/views'); //Aplikazioaren views karpeta edozein lekutatik deitzeko
     define('HREF_SRC_DIR', '/GarapenIngurunea/ProjectManager/WEB/holanda-fantasy/WEB3/src');
+    ?>
--- WEB3/src/views/guriBuruz/guriBuruz.php
+++ WEB3/src/views/guriBuruz/guriBuruz.php
@@ -4,7 +4,23 @@ require_once("../../required/head.php");
 ?>
 
 <link rel="stylesheet" href="<?= HREF_SRC_DIR ?>/views/guriBuruz/guriBuruz.css">
-guriBuruz
+<body>
+    <section>
+        <h3>Sobre nosotros</h3>
+        <p>Bienvenido a Project Manager, donde convertimos ideas en experiencias digitales innovadoras. Con sede en <a href="https://maps.app.goo.gl/HCrM4Fo4H9dGgZ6h7" target="_blank">Goierri</a>, somos un equipo apasionado de desarrolladores, diseñadores y estrategas dedicados a impulsar el éxito de tu empresa a través de soluciones tecnológicas personalizadas.</p>
+    </section>
+    
+    <section>
+        <h3>Nuestra Misión</h3>
+        <p>En Project Manager, nuestra misión es transformar la visión de tu empresa en realidad digital. Creemos en el poder de la tecnología para impulsar el crecimiento empresarial y mejorar la experiencia del usuario. Nos esforzamos por ofrecer soluciones de software de vanguardia que no solo satisfagan las necesidades actuales de tu empresa, sino que también preparen el camino para el éxito futuro.</p>
+    </section>
+    
+    <section>
+        <h3>Nuestro Enfoque</h3>
+        <p>Nos especializamos en el desarrollo de aplicaciones móviles y software a medida para empresas de todos los tamaños y sectores. Ya sea que estés buscando una aplicación móvil intuitiva para tus clientes o una plataforma empresarial compleja para optimizar tus procesos internos, nuestro equipo tiene la experiencia y la creatividad necesarias para hacerlo realidad.</p>
+    </section>
+</body>
+
 <?php
 require_once("../../required/footer.php");
 ?>
